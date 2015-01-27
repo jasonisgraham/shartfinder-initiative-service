71df7eb5306b3719adb0a6e2bb73b917cd24374e
Jason Graham <jason.graham@summa-tech.com>
2015-01-27 16:50:26 -0500

    committing today's changes

--- src/shartfinder_initiative_service/core.clj
+++ src/shartfinder_initiative_service/core.clj
@@ -1,7 +1,9 @@
 (ns shartfinder-initiative-service.core
   (:require [taoensso.carmine :as car :refer (wcar)]
             [clojure.data.json :as json]
-            [shartfinder-initiative-service.dice-roller :as dice-roller])
+            [shartfinder-initiative-service.dice-roller :as dice-roller]
+            [shartfinder-initiative-service.combatant-service :as combatant-service]
+            [shartfinder-initiative-service.utils :as initiative-utils])
   (:gen-class))
 
 (def server-connection {:pool {}
@@ -10,45 +12,69 @@
                                :password "abc123"}})
 
 (def channels {:encounter-created "encounter-created"
+               :create-initiative "roll-initiative"
                :initiative-created "initiative-created"})
 
 (defmacro wcar* [& body] `(car/wcar server-connection ~@body))
 
+;; I'm using atoms b/c I don't know clojure very well don't know no better :(
+(def characters-rolled (atom {}))
+(def characters-received (atom []))
+
 (defn get-players-from-json [content-json]
-  (concat (content-json "gmCombatants") (content-json "playerCombatants")))
+  "accepts gmCombatants, playerCombatants, combatants"
+  (concat (content-json "gmCombatants") (content-json "playerCombatants") (content-json "combatants")))
 
-(defn zippy [l1 l2]
-  "like zipmap but doesn't creates sets for duplicates keys
-   http://stackoverflow.com/questions/17134771/zipmap-with-multi-value-keys"
-  (apply merge-with concat (map (fn [a b]{a (list b)}) l1 l2)))
+(defn get-initiative [player-id dice-roll]
+  "TODO get initiative-bonuses when :encounter-created event is picked up, then use that instead of getting everyone at the end"
+  (+ dice-roll (combatant-service/get-initiative-bonus player-id)))
 
-;; This function is balls ugly and when I learn clojure better, hoping to pretty it up
-;; TODO this does not resolve collision with same dice num.  thinking of shuffling val set by keys 1-20
-(defn create-initiative
-  ([content-json]
-   (let [players (get-players-from-json content-json)
-         dice-outcomes (take (count players) (repeatedly #(dice-roller/roll-dice nil)))]
-     (create-initiative content-json dice-outcomes)))
-  ([content-json dice-outcomes]
-   ;; the dice-outcomes option is only here b/c I dont know how to mock out functions when testing
-   (let [players (get-players-from-json content-json)
-         m (into (sorted-map) (zippy dice-outcomes players))
-         ordered-players (reverse m)]
-     (flatten (vals ordered-players)))))
+(defn create-initiative [json-string]
+  )
 
-(defn- create-and-publish-initiative [content-json]
-  (let [initiative-json (->> content-json
-                             (json/read-str)
-                             (create-initiative)
-                             (hash-map :order)
-                             (json/write-str))]
-    (wcar* (car/publish (channels :initiative-created) initiative-json))))
+(defn process-single-initiative [initiative-json-string]
+  "Accepts json String with EncounterId, PlayerId, & DiceValue"
+  (let [initiative-json (json/read-str initiative-json-string)
+        player-id (initiative-json "playerId")
+        initiative (get-initiative player-id (initiative-json "diceRoll"))]
+    ;; FIXME im not actually verifying anything!
+    (swap! characters-rolled assoc player-id initiative)))
+
+(defn process-initiative-created [content-json]
+  "FIXME not yet implemented"
+  (if (<= (count @characters-received) (count @characters-rolled))
+    (wcar@ (car/public (channels :initiative-created) order-json))))
 
 (def listener
   (car/with-new-pubsub-listener (:spec server-connection)
     {(channels :encounter-created) (fn f1 [[type match content-json :as payload]]
                                      (when (instance? String content-json)
-                                       (create-and-publish-initiative content-json)))}
+                                       (swap! characters-received (get-players-from-json (json/read-str content-json)))))
+     (channels :create-initiative) (fn f2 [[type match content-json :as payload]]
+                                      (process-initiative-created content-json))}
     (car/subscribe (channels :encounter-created))))
+
+
+;; This function is balls ugly and when I learn clojure better, hoping to pretty it up
+;; TODO this does not resolve collision with same dice num.  thinking of shuffling val set by keys 1-20
+;; (defn create-initiative
+;;   ([content-json]
+;;    (let [players (get-players-from-json content-json)
+;;          dice-outcomes (take (count players) (repeatedly #(dice-roller/roll-dice nil)))]
+;;      (create-initiative content-json dice-outcomes)))
+;;   ([content-json dice-outcomes]
+;;    ;; the dice-outcomes option is only here b/c I dont know how to mock out functions when testing
+;;    (let [players (get-players-from-json content-json)
+;;          m (into (sorted-map) (initiative-utils/zippy dice-outcomes players))
+;;          ordered-players (reverse m)]
+;;      (flatten (vals ordered-players)))))
+
+;; (defn- create-and-publish-initiative [content-json]
+;;   (let [initiative-json (->> content-json
+;;                              (json/read-str)
+;;                              (create-initiative)
+;;                              (hash-map :order)
+;;                              (json/write-str))]
+;;     (wcar* (car/publish (channels :initiative-created) initiative-json))))
 
 (defn -main [])
