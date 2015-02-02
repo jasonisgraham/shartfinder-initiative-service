58d481862c62cf883e98dee6ae72ba0e65219bf6
Jason Graham <jason.graham@summa-tech.com>
2015-02-01 22:44:12 -0500

    revert last change i think cuz things will not deploy

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject shartfinder-initiative-service "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -13,4 +12,4 @@
   :uberjar-name "shartfinder-initiative-service-standalone.jar"
   :aot [shartfinder-initiative-service.web])
 
-;; web: java $JVM_OPTS -cp target/shartfinder-initiative-service-standalone.jar clojure.main -m shartfinder-initiative-service.core
+;;  :min-lein-version "2.0.0"
