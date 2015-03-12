2560bff6463626f38a4ac4552f3b3fb6118157b0
Jason Graham <jason.graham@summa-tech.com>
2015-03-11 23:53:43 -0400

    updating for new payload schema

--- src/shartfinder_initiative_service/core.clj
+++ src/shartfinder_initiative_service/core.clj
@@ -19,89 +19,83 @@
                          ;; TODO should this be an error only API gateway listens to??
                          :error "error"})
 
-(def ^:private identifiers {:combatant-id :playerId
-                            :dice-roll :diceRoll
-                            :initiative-bonus :initiativeBonus})
-
 (defmacro wcar* [& body]
   `(car/wcar server-connection ~@body))
 
-;; I need to add something that allows these to be reset if nessy, or (better yet), just include encounter-id
 (defonce combatants-rolled (atom {}))
-(defonce combatants-received (atom #{}))
-(defonce ordered-initiative (atom nil))
+(defonce combatants-received (atom {}))
+(defonce ordered-initiative (atom []))
 (defonce encounter-id (atom nil))
 
-(defn get-combatants-from-json [ content]
-  "accepts gmCombatants, playerCombatants, combatants"
-  (concat (content :gmCombatants) (content :playerCombatants) (content :combatants)))
-
-(defn get-initiative-bonus [combatant-id]
-  (let [url (str (service-urls :combatant) "/initiative-bonus/id")
+(defn get-initiative-bonus [combatant-name]
+  (let [url (str (service-urls :combatant) "/initiative-bonus/combatant-name")
         response (client/get url {:throw-exceptions false})]
     (if (= (:status response) 200)
-      (->> (:initiative-bonus identifiers) ((json/read-str (:body response) :key-fn keyword)) (Integer/parseInt))
-      ;; TODO probably better way to produce this
+      (->> :initiativeBonus ((json/read-str (:body response) :key-fn keyword)) (Integer/parseInt))
       (wcar* (car/publish (channels :error) (json/write-str {:response (:status response)
                                                              :url url
                                                              :source "initiative-service"}))))))
 
-(defn get-initiative [combatant-id dice-roll]
-  (let [initiative-bonus (get-initiative-bonus combatant-id)]
-    (+ dice-roll initiative-bonus)))
+(defn get-initiative-value [combatant-info]
+  (let [initiative-bonus (get-initiative-bonus combatant-info)]
+    (+ (:diceRoll combatant-info) initiative-bonus)))
 
-(defn create-initiative [unordered-initiative-map]
-  (let [combatants (keys unordered-initiative-map)
-        initiatives (vals unordered-initiative-map)
+(defn create-initiative [unordered-combatants]
+  (let [combatants (keys unordered-combatants)
+        initiatives (map #(:initiative %) (vals unordered-combatants))
         m (into (sorted-map) (initiative-utils/zippy initiatives combatants))
-        ordered-combatants (reverse m)]
-    (flatten (vals ordered-combatants))))
+        ordered-combatants (-> m reverse vals flatten) ]
+    (into {} (map #(assoc {} % (unordered-combatants %)) ordered-combatants))))
 
-(defn should-allow-combatant-roll? [combatant-id]
-  "returns true if combatant-id is in combatants-received & not in combatants-rolled"
-  (and (some #{combatant-id} @combatants-received)
-       (nil? (@combatants-rolled combatant-id))))
+(defn is-combatant-in-combatants-received? [combatant-info]
+  (not (nil? (@combatants-received (:combatantName combatant-info)))))
 
-(defn process-single-initiative [initiative]
-  "Accepts json String with EncounterId, CombatantId, & DiceValue"
-  (let [combatant-id ((identifiers :combatant-id) initiative)]
-    (when (should-allow-combatant-roll? combatant-id)
-      (let [initiative (get-initiative combatant-id (initiative (identifiers :dice-roll)))]
-        (swap! combatants-rolled assoc combatant-id initiative)))))
+(defn has-combatant-rolled? [combatant-info]
+  (not (nil? (@combatants-rolled (:combatantName combatant-info)))))
+
+(defn should-allow-combatant-roll? [combatant]
+  "returns true if combatant-name is in combatants-received & not in combatants-rolled"
+  (and (is-combatant-in-combatants-received? combatant) (not (has-combatant-rolled? combatant))))
+
+(defn process-single-initiative [combatant-info]
+  "If combatant roll is accepted, update @combatants-rolled"
+  (let [combatant-name (:combatantName combatant-info)]
+    (when (should-allow-combatant-roll? combatant-info)
+      (let [initiative-value (get-initiative-value combatant-info)]
+        (swap! combatants-rolled assoc (:combatantName combatant-info) (assoc combatant-info :initiative initiative-value))))))
 
 (defn who-hasnt-rolled? []
-  (set/difference @combatants-received (keys @combatants-rolled)))
+  (set/difference (set (keys @combatants-received)) (set (keys @combatants-rolled))))
 
 (defn- has-everyone-rolled? []
   (>= (count @combatants-rolled) (count @combatants-received)))
 
-(defn process-initiative-created [ content-json]
-  (process-single-initiative  content-json)
+(defn process-initiative-created [combatant-info]
+  "process initiative roll, then if everyone has rolled, order initiative then publish"
+  (process-single-initiative combatant-info)
 
   (when (has-everyone-rolled?)
-    (let [ordered-combatant-ids (create-initiative @combatants-rolled)]
-      (reset! ordered-initiative {:encounterId @encounter-id, :orderedCombatantIds ordered-combatant-ids})
-      ;; returns the json payload (this will help with testing)
-      ;; I'm hoping there is a better way to test this redis stuff
+    (let [ordered-combatants (create-initiative @combatants-rolled)]
+      (reset! ordered-initiative {:encounterId @encounter-id, :orderedCombatants ordered-combatants})
+      ;; returns the json payload (this will help with testing) I'm hoping there is a better way to test this redis stuff
       (let [publish-str (json/write-str @ordered-initiative)]
         (wcar* (car/publish (channels :initiative-created) publish-str))
-        publish-str))))
+        publish-str)))
+  )
 
-(defn initialize-encounter-id [combatant-json]
-  (reset! encounter-id (-> combatant-json (get :encounterId))))
-
-(defn initialize-received-combatants [combatant-json]
-  (initialize-encounter-id combatant-json)
-  (->> combatant-json  (get-combatants-from-json) (set) (reset! combatants-received)))
+(defn initialize-received-combatants [combatants-info]
+  "TODO what's the better way to do this?"
+  (reset! encounter-id (combatants-info :encounterId))
+  (reset! combatants-received (into {} (map #(assoc {} (:combatantName %) %) (:combatants combatants-info)))))
 
 (defonce listener
   (car/with-new-pubsub-listener (:spec server-connection)
     {(channels :encounter-created) (fn f1 [[type match  content-json :as payload]]
                                      (when (instance? String  content-json)
-                                       (initialize-received-combatants  content-json)))
+                                       (initialize-received-combatants (json/read-str content-json :key-fn keyword))))
      (channels :initiative-rolled) (fn f2 [[type match  content-json :as payload]]
                                      (when (instance? String  content-json)
-                                       (process-initiative-created (json/read-str content-json))))}
+                                       (process-initiative-created (json/read-str content-json :key-fn keyword))))}
     (car/subscribe (channels :encounter-created) (channels :initiative-rolled))))
 
 (defn get-response []
--- test/shartfinder_initiative_service/core_test.clj
+++ test/shartfinder_initiative_service/core_test.clj
@@ -13,17 +13,40 @@
 ;;   nil
 ;;   )
 
-;; (comment
-;;   need to add UserId & CombatantName
-;;   expect these from encounter service
-;;   these need to be in published payloads
-;;   ;; {"encounterId": 69, "orderedCombatantIds": [{"userId":"Jason", "combatantName":"SilverNoun"},{"userId":"Jim", "combatantName":"Goblin 1"},{"userId":"Jim", "combatantName":"Goblin 1"}]}
-;;   )
+(comment
+  The service responds with something like this
+  {:encounterId 69, :orderedCombatants [{:userId "Jason", :combatantName "SilverNoun"}
+                                        {:userId "Jim", :combatantName "Goblin 1"}
+                                        {:userId "Dogman", :combatantName "Mean Mouth"}]}
 
+  subscribes to "encounter-created" which looks like this
+  {:encounterId 69, :combatants [ {:userId "jim", :combatantName "DTL Boss"}, {:userId "boolnerbroni", :combatantName "Grook"}] })
+
+(defn create-test-combatant
+  ([user-id combatant-name]
+   {:userId user-id, :combatantName combatant-name})
+  ([user-id combatant-name dice-roll]
+   {:userId user-id, :combatantName combatant-name, :diceRoll dice-roll}))
+
+(defn create-silver-noun
+  ([]
+   (create-test-combatant "jason" "SilverNoun"))
+  ([dice-roll]
+   (create-test-combatant "jason" "SilverNoun" dice-roll)))
+
+(defn create-mean-mouth
+  ([]
+   (create-test-combatant "dogman" "MeanMouth"))
+  ([dice-roll]
+   (create-test-combatant "dogman" "MeanMouth" dice-roll)))
+
+(defn- reset-atoms! []
+  (reset! combatants-rolled {})
+  (reset! combatants-received {})
+  (reset! encounter-id nil))
 
 (defn my-test-fixture [f]
-  (reset! combatants-rolled {})
-  (reset! combatants-received #{})
+  (reset-atoms!)
   (f))
 
 (use-fixtures :each my-test-fixture)
@@ -33,132 +56,204 @@
   (is (= 0 (count @combatants-received))))
 
 (deftest test-that-combatant-service-can-be-hit
-  (is (not (nil? (get-initiative-bonus "dogman")))))
+  (is (not (nil? (get-initiative-bonus (create-mean-mouth))))))
 
 (deftest test-get-correct-initiative
-  (with-redefs [get-initiative-bonus (fn [id] "25")]
-    (is (= "25" (get-initiative-bonus "dogman")))))
+  (with-redefs [get-initiative-bonus (fn [id] 25)]
+    (is (= 25 (get-initiative-bonus (create-mean-mouth))))))
 
 (deftest test-get-initiative
   (with-redefs [get-initiative-bonus (fn [id] -1)]
-    (is (= 17 (get-initiative "jason" 18))))
+    (is (= 17 (get-initiative-value (create-silver-noun 18)))))
+
   (with-redefs [get-initiative-bonus (fn [id] 2)]
-    (is (= 20 (get-initiative "jason" 18)))
-    (is (= 9 (get-initiative "jason" 7)))))
+    (is (= 20 (get-initiative-value (create-silver-noun 18))))
+    (is (= 9 (get-initiative-value (create-silver-noun 7))))))
 
 (deftest test-process-single-initiative
-  (reset! combatants-received #{"jason" "dogman"})
-  (let [char-1-json {:playerId "jason", :diceRoll 13}
-        char-2-json {:playerId "dogman", :diceRoll 7}]
+  (initialize-received-combatants {:encounterId 69,
+                                   :combatants [{:userId "jason", :combatantName "SilverNoun"},
+                                                {:userId "dogman", :combatantName "MeanMouth"}]})
 
-    (with-redefs [get-initiative-bonus (fn [id] 3)]
-      (process-single-initiative char-1-json))
-    (is (= {"jason" 16} @combatants-rolled))
+  (with-redefs [get-initiative-bonus (fn [id] 0)]
+    (process-single-initiative {:userId "jason" :combatantName "SilverNoun" :diceRoll 13}))
 
-    (with-redefs [get-initiative-bonus (fn [id] -2)]
-      (process-single-initiative char-2-json))
-    (is (= {"jason" 16, "dogman" 5} @combatants-rolled))))
+  (is (= '("SilverNoun") (keys @combatants-rolled)))
+  (let [silver-noun-actual (@combatants-rolled "SilverNoun")]
+    (is (= 13 (:initiative silver-noun-actual)))
+    (is (= "SilverNoun" (:combatantName silver-noun-actual)))
+    (is (= "jason" (:userId silver-noun-actual)))
+    (is (= 13 (:diceRoll silver-noun-actual))))
 
-(deftest test-process-single-combatant-one-roll-one-character
-  (reset! combatants-received #{"jason"})
-  (with-redefs [get-initiative-bonus (fn [id] -2)]
-    (is (= {"jason" 11}
-           (process-single-initiative {:playerId "jason", :diceRoll 13})))))
+  (with-redefs [get-initiative-bonus (fn [id] 3)]
+    (process-single-initiative {:userId "dogman", :combatantName "MeanMouth" :diceRoll 7}))
+  (is (= '("MeanMouth" "SilverNoun") (keys @combatants-rolled)))
+
+  (let [mean-mouth-actual (@combatants-rolled "MeanMouth")]
+    (is (= 10 (:initiative mean-mouth-actual)))
+    (is (= "MeanMouth" (:combatantName mean-mouth-actual)))
+    (is (= "dogman" (:userId mean-mouth-actual)))
+    (is (= 7 (:diceRoll mean-mouth-actual)))))
+
+;; (deftest test-process-single-combatant-one-roll-one-character
+;;   )
+
+(deftest test-should-allow-combatant-roll?
+  (let [silver-noun {:userId "jason" :combatantName "SilverNoun" :diceRoll 11}]
+    (testing "is-combatant-in-combatants-received? no"
+      (reset-atoms!)
+      (is (= false (is-combatant-in-combatants-received? silver-noun))))
+
+    (testing "is-combatant-in-combatants-received? yes"
+      (reset-atoms!)
+      (swap! combatants-received assoc "SilverNoun" silver-noun)
+      (is (= true (is-combatant-in-combatants-received? silver-noun))))
+
+    (testing "has-combatant-already-rolled? no"
+      (reset-atoms!)
+      (is (= 0 (count @combatants-rolled)))
+      (is (= false (has-combatant-rolled? silver-noun))))
+
+    (testing "has-combatant-already-rolled? yes"
+      (reset-atoms!)
+      (swap! combatants-rolled assoc "SilverNoun" silver-noun)
+      (is (= true (has-combatant-rolled? silver-noun))))
+
+    (testing "shouldn't allow - DNE in combatants-received"
+      (reset-atoms!)
+      (is (= 0 (count @combatants-received)))
+      (is (= false (should-allow-combatant-roll? silver-noun))))
+
+    (testing "should allow"
+      (reset-atoms!)
+      (is (= 0 (count @combatants-received)))
+      (swap! combatants-received assoc "SilverNoun" silver-noun)
+      (is (= 1 (count @combatants-received)))
+      (is (= true (should-allow-combatant-roll? silver-noun))))
+
+    (testing "shouldn't allow - already added/exists in combatants-rolled"
+      (reset-atoms!)
+      (swap! combatants-rolled assoc "SilverNoun" silver-noun)
+      (swap! combatants-received assoc "SilverNound" silver-noun)
+      (is (= 1 (count @combatants-rolled)))
+      (is (= 1 (count @combatants-received)))
+      (is (= false (should-allow-combatant-roll? silver-noun))))
+    ))
+
 
 (deftest test-create-initiative
-  (let [unordered-initiative-map {"jason" 10, "goblin" 20, "dragon" 13, "silverstank" 18}]
-    (testing "with no collistions"
-      (is (= ["goblin" "silverstank" "dragon" "jason"] (create-initiative unordered-initiative-map))))
-    (testing "with collisions at end"
-      (let [tied-with "jason"
-            tied-name "someBimbo"
-            actual-initiative (create-initiative (assoc unordered-initiative-map tied-name (unordered-initiative-map tied-with)))]
-        (is (= "goblin" (nth actual-initiative 0)))
-        (is (= "silverstank" (nth actual-initiative 1)))
-        (is (= "dragon" (nth actual-initiative 2)))
-        (is (= #{tied-with tied-name} (set (take-last 2 actual-initiative))))))
-    (testing "with collisions at beginning"
-      (let [tied-with "goblin"
-            tied-name "someBimbo"
-            actual-initiative (create-initiative (assoc unordered-initiative-map tied-name (unordered-initiative-map tied-with)))]
-        (is (= "jason" (nth actual-initiative 4)))
-        (is (= "silverstank" (nth actual-initiative 2)))
-        (is (= "dragon" (nth actual-initiative 3)))
-        (is (= #{tied-with tied-name} (set (take 2 actual-initiative))))))))
+  (let [stankypants {:userId "boolner" :combatantName "stankypants" :initiative 18}
+        silverparts {:userId "jason"   :combatantName "silverparts" :initiative 5}
+        meanmouth   {:userId "dogman"  :combatantName "MeanMouth" :initiative 13}
+        dragon      {:userId "jim"     :combatantName "dragon" :initiative 17}
+
+        unordered-combatants {"stankypants" stankypants
+                              "dragon" dragon
+                              "meanmouth" meanmouth
+                              "silverparts" silverparts}
+
+        expected {"stankypants" stankypants
+                  "dragon" dragon
+                  "meanmouth" meanmouth
+                  "silverparts" silverparts}]
+
+    (testing "with no collisions"
+      (is (= expected (create-initiative unordered-combatants))))))
+
+;; (testing "with collisions at end"
+;;   (let [tied-with "jason"
+;;         tied-name "someBimbo"
+;;         actual-initiative (create-initiative (assoc unordered-initiative tied-name (unordered-initiative tied-with)))]
+;;     (is (= "goblin" (nth actual-initiative 0)))
+;;     (is (= "silverstank" (nth actual-initiative 1)))
+;;     (is (= "dragon" (nth actual-initiative 2)))
+;;     (is (= #{tied-with tied-name} (set (take-last 2 actual-initiative))))))
+
+;; (testing "with collisions at beginning"
+;;   (let [tied-with "goblin"
+;;         tied-name "someBimbo"
+;;         actual-initiative (create-initiative (assoc unordered-initiative tied-name (unordered-initiative tied-with)))]
+;;     (is (= "jason" (nth actual-initiative 4)))
+;;     (is (= "silverstank" (nth actual-initiative 2)))
+;;     (is (= "dragon" (nth actual-initiative 3)))))
 
 (deftest test-who-hasnt-rolled
-  (reset! combatants-received #{"stankypants" "dogman" "dragon" "goblin" "someDude"})
+  (reset! combatants-received {"stankypants" {:userId "boolner" :combatantName "stankypants"},
+                               "MeanMouth"   {:userId "dogman"  :combatantName "MeanMouth"},
+                               "dragon"      {:userId "jim"     :combatantName "dragon"},
+                               "someDude"    {:userId "shook"   :combatantName "someDude"},
+                               "goblin1"     {:userId "jim"     :combatantName "goblin1"}})
   (testing "no one's rolled yet"
-    (is (= #{"stankypants" "dogman" "dragon" "goblin" "someDude"} (who-hasnt-rolled?))))
+    (is (= #{"goblin1" "stankypants" "MeanMouth" "dragon" "someDude"} (set (who-hasnt-rolled?)))))
+
   (testing "goblin has rolled"
-    (reset! combatants-rolled {"goblin" 10})
-    (is (= @combatants-rolled {"goblin" 10}))
-    (is (= #{"stankypants" "dogman" "dragon" "someDude"} (who-hasnt-rolled?))))
+    (reset! combatants-rolled {"goblin1" {:userId "jim", :combatantName "goblin1", :diceRoll 10}})
+    (let [actual (who-hasnt-rolled?)]
+      (is (= #{"stankypants" "MeanMouth" "dragon" "someDude"} actual))))
+
   (testing "everyone has rolled"
-    (reset! combatants-rolled {"goblin" 10, "stankypants" 2, "dogman" 3, "dragon" 33, "someDude" 18})
+    (reset! combatants-rolled {"goblin1" {} "stankypants" {} "MeanMouth" {} "dragon" {} "someDude" {}})
     (is (= #{} (who-hasnt-rolled?)))))
 
 (deftest test-process-initiative-created
-  (reset! combatants-received #{"stankyPants" "dogman" "someDude"})
-  (reset! encounter-id 69)
-  (let [jason {:playerId "jason", :diceRoll 13}
-        goblin {:playerId "goblin", :diceRoll 20}
-        dogman {:playerId "dogman", :diceRoll 9}
-        stanky-pants {:playerId "stankyPants", :diceRoll 4}
-        some-dude {:playerId "someDude", :diceRoll 8}
-        expected (json/write-str {:encounterId 69, :orderedCombatantIds ["dogman" "someDude" "stankyPants"]}) ]
-    (is (= nil (process-initiative-created jason)))
-    (is (= nil (process-initiative-created goblin)))
-    (is (= nil (process-initiative-created dogman)))
-    (is (= nil (process-initiative-created some-dude)))
-    (is (= expected (process-initiative-created stanky-pants)))))
+  (let [stankypants {:userId "boolner" :combatantName "stankypants" :diceRoll 18}
+        meanmouth   {:userId "dogman"  :combatantName "meanmouth" :diceRoll 13}
+        dragon      {:userId "jim"     :combatantName "dragon" :diceRoll 17}
+        silverparts {:userId "jason"   :combatantName "silverparts" :diceRoll 5}]
+    (reset! combatants-received {"stankypants" stankypants
+                                 "meanmouth" meanmouth
+                                 "dragon" dragon
+                                 "silverparts" silverparts})
+    (reset! encounter-id 69)
+
+    (let [expected (json/write-str {:encounterId 69
+                                    :orderedCombatants {"stankypants" (assoc stankypants :initiative 20 :diceRoll 18)
+                                                        "dragon" (assoc dragon :initiative 19 :diceRoll 17)
+                                                        "meanmouth" (assoc meanmouth :initiative 15 :diceRoll 13)
+                                                        "silverparts" (assoc silverparts :initiative 7 :diceRoll 5)}})]
+      (with-redefs [get-initiative-bonus (fn [id] 2)]
+        (is (= nil (process-initiative-created meanmouth)))
+        (is (= nil (process-initiative-created dragon)))
+        (is (= nil (process-initiative-created silverparts)))
+        (is (= nil (process-initiative-created dragon)))
+        (is (= expected (process-initiative-created stankypants)))))))
 
 (deftest test-initialize-received-combatants
-  (testing "only gmCombatants"
-    (reset! combatants-received #{})
-    (initialize-received-combatants {:gmCombatants ["jason" "dogman" "fartman"] :playerCombatants []})
-    (is (= #{"jason" "dogman" "fartman"} (set @combatants-received))))
+  (let [encounter-created-payload {:encounterId 69,
+                                   :combatants [{:userId "jim", :combatantName "DTL Boss"},
+                                                {:userId "boolnerbroni", :combatantName "Grook"}] }]
+    (reset-atoms!)
+    (initialize-received-combatants encounter-created-payload)
+    (is (= {"DTL Boss" {:userId "jim", :combatantName "DTL Boss"}
+            "Grook"    {:userId "boolnerbroni", :combatantName "Grook"}} @combatants-received))
+    (is (= 69 @encounter-id))))
 
-  (testing "only playerCombatants"
-    (reset! combatants-received #{})
-    (initialize-received-combatants {:playerCombatants ["jason" "dogman" "fartman"] :gmCombatants []})
-    (is (= #{"jason" "dogman" "fartman"} (set @combatants-received))))
-
-  (testing "only combatants"
-    (reset! combatants-received #{})
-    (initialize-received-combatants {:combatants ["jason" "dogman" "fartman"] :gmCombatants []})
-    (is (= #{"jason" "dogman" "fartman"} (set @combatants-received))))
-
-  (testing "all 3 combatants"
-    (reset! combatants-received #{})
-    (initialize-received-combatants {:gmCombatants ["jason"] :playerCombatants ["fartman"]  :combatants ["dogman"]})
-    (is (= #{"jason" "dogman" "fartman"} (set @combatants-received)))))
 
 (deftest test-that-only-first-roll-counts
-  (reset! combatants-received #{})
+  (reset-atoms!)
 
   (with-redefs [get-initiative-bonus (fn [id] 0)]
+    (let [silver-noun {:userId "jason", :combatantName "SilverNoun"}
+          mean-mouth {:userId "dogman", :combatantName "MeanMouth"}
+          get-dice-roll (fn [needle] (some #(if (= (:combatantName %) needle) %) @combatants-received))]
 
-    (initialize-received-combatants {:combatants ["dogman", "fartman"]})
-    (is (= 2 (count @combatants-received)))
-    (is (= nil (@combatants-rolled "dogman")))
+      (initialize-received-combatants {:combatants [mean-mouth, silver-noun]})
 
-    (process-single-initiative {:playerId "dogman", :diceRoll 13})
-    (is (= 13 (@combatants-rolled "dogman")))
+      (is (= 2 (count @combatants-received)))
+      (is (= nil (@combatants-rolled "dogman")))
 
-    (process-single-initiative {:playerId "dogman", :diceRoll 19})
-    (is (= 13 (@combatants-rolled "dogman")))))
+      (process-single-initiative (assoc silver-noun :diceRoll 13))
 
-(deftest test-that-unknown-player-cant-roll-initiative
-  (reset! combatants-received #{})
+      (is (= 13 (:initiative (@combatants-rolled "SilverNoun"))))
 
-  (initialize-received-combatants {:combatants ["dogman"]})
-  (is (= nil (@combatants-rolled "dogman")))
+      (process-single-initiative {:userId "dogman", :combatantName "SilverNoun", :diceRoll 19})
+      (is (= 13 (:initiative (@combatants-rolled "SilverNoun")))))))
 
-  (process-single-initiative {:playerId "fartman" :diceRoll 19})
-  (is (= nil (@combatants-rolled "fartman"))))
+;; (deftest test-that-unknown-player-cant-roll-initiative
+;;   (reset-atoms!))
 
 (deftest test-encounter-id
+  (reset-atoms!)
   (testing "is initially nil"
     (is (nil? @encounter-id)))
 
@@ -166,3 +261,19 @@
     (let [combatants-json {:encounterId 69, :gmCombatants ["jason"], :playerCombatants ["fartman"], :combatants ["dogman"]}]
       (initialize-received-combatants combatants-json)
       (is (= 69 @encounter-id)))))
+
+(deftest test-published-messages-are-picked-up-correctly
+  "These are hacked with a wait for half a second. "
+
+  (let [encounter-created-payload {:encounterId 69,
+                                   :combatants [{:userId "jim", :combatantName "DTL Boss"},
+                                                {:userId "boolnerbroni", :combatantName "Grook"}] }]
+
+    (testing "with encounter-created: exists no initial combatants"
+      (Thread/sleep 500)
+      (is (= 0 (count @combatants-received))))
+
+    (testing "with encounter-created exist 2 combatants"
+      (wcar* (car/publish "encounter-created" (json/write-str encounter-created-payload)))
+      (Thread/sleep 500)
+      (is (= 2 (count @combatants-received))))))
